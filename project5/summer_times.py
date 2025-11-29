97c393c44ec3dba2411d87dd09f6a7365a00600a
Iamparastish <akhtrilundi85@gmail.com>
2025-11-29 13:50:45 +0500

    This is project No. 05

---
+++ project5/summer_times.py
@@ -0,0 +1,8 @@
+month = input("Enter the month name : ") . lower()
+
+if month == "augest" or month == "june" or month == "july" :
+    print("It is summer time!")
+elif month == "april" or month == "may":
+     print("It is almost summer.")
+else:
+    print("It is not summer time.")
